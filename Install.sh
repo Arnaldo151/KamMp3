99d8d7fed050877c15823b5b785dcad73ae1975b
KANEKIMODS <83626584+KANEKIMOD@users.noreply.github.com>
2023-11-19 18:52:27 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 lineas="==================================="
 red='\033[1;31m'
-green='\033[1;32m'
-cyan='\033[1;36m'
 reset='\033[0m'
 
 printf $red
@@ -17,33 +15,19 @@ printf $reset
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$script_dir"
 
-# Detect if Termux is present
+# Check if Termux is present
 if command -v termux-info > /dev/null 2>&1; then
-    os="termux"
+    pkg update -y
+    pkg install -y ffmpeg
+    pkg install -y python
+    pip install yt-dlp
+    chmod +x KanMp3
+    printf $red
+    echo $lineas
+    echo "Instalación finalizada. Para iniciar la herramienta ejecuta ./KanMp3"
+    printf $reset
 else
-    os="ubuntu"
+    printf $red
+    echo "Este script está diseñado para Termux. No se detectó Termux en el sistema."
+    printf $reset
 fi
-
-case $os in
-    termux)
-        pkg update -y
-        pkg install -y ffmpeg
-        pkg install -y python
-        pip install yt-dlp
-        chmod +x KanMp3
-        printf $green
-        echo $lineas
-        echo "Instalación finalizada. Para iniciar la herramienta ejecuta ./KanMp3"
-        printf $reset
-        ;;
-    ubuntu)
-        sudo apt update -y
-        sudo apt install -y ffmpeg
-        sudo apt install -y yt-dlp
-        chmod +x KanMp3
-        printf $cyan
-        echo $lineas
-        echo "Instalación finalizada. Para iniciar la herramienta ejecuta ./KanMp3"
-        printf $reset
-        ;;
-esac
